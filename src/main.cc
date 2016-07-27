5bda8d61b24128c874e48e0da54ef94e0f546598
cbpark <cbpark@gmail.com>
2016-07-28 02:01:26 +0900

    Tests

--- src/main.cc
+++ src/main.cc
@@ -1,9 +1,21 @@
 #include "main.h"
 
+#include <algorithm>
+#include <cctype>
 #include <fstream>
 #include <iostream>
 #include <sstream>
 #include <string>
+
+int numInitSpaces(const std::string& str) {
+    int num = 0;
+    for (const auto& c: str) {
+        if (std::isspace(c)) {
+            ++num;
+        }
+    }
+    return num;
+}
 
 int main(int argc, char* argv[]) {
     std::string appname = "test";
@@ -16,8 +28,24 @@ int main(int argc, char* argv[]) {
     std::ifstream infile(argv[1]);
     std::string line;
     while (std::getline(infile, line)) {
-        std::istringstream iss(line);
-        std::cout << iss.str() << '\n';
+        if (line.find("#") == std::string::npos) {
+            std::istringstream iss(line);
+            std::cout << iss.str() << '\n';
+        } else {
+            continue;
+        }
     }
     infile.close();
+
+    std::istringstream test_str1("sigma: data/Sigma.dat");
+    std::string parsed;
+    while (std::getline(test_str1, parsed, ':')) {
+        parsed.erase(std::remove(parsed.begin(), parsed.end(), ' '), parsed.end());
+        std::cout << parsed << '\n';
+    }
+
+    std::istringstream test_str2("sigma:\n  data/Sigma.dat");
+    while (std::getline(test_str2, parsed)) {
+        std::cout << numInitSpaces(parsed) << "  " << parsed << '\n';
+    }
 }
