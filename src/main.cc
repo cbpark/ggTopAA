d6a5311d5d040fd679a03f609ed15e8aa65541fc
cbpark <cbpark@gmail.com>
2016-07-28 03:50:42 +0900

    Tests (2)

--- src/main.cc
+++ src/main.cc
@@ -4,17 +4,28 @@
 #include <cctype>
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <sstream>
 #include <string>
 
 int numInitSpaces(const std::string& str) {
     int num = 0;
-    for (const auto& c: str) {
-        if (std::isspace(c)) {
+    for (const auto& c : str) {
+        if (std::isblank(c)) {
             ++num;
         }
     }
     return num;
+}
+
+std::string stripSpaces(const std::string& str) {
+    std::string result;
+    for (const auto& c : str) {
+        if (!std::isblank(c) && !(c == '-')) {
+            result += c;
+        }
+    }
+    return result;
 }
 
 int main(int argc, char* argv[]) {
@@ -37,15 +48,24 @@ int main(int argc, char* argv[]) {
     }
     infile.close();
 
-    std::istringstream test_str1("sigma: data/Sigma.dat");
+    // std::istringstream test_str1("sigma: data/Sigma.dat");
     std::string parsed;
-    while (std::getline(test_str1, parsed, ':')) {
-        parsed.erase(std::remove(parsed.begin(), parsed.end(), ' '), parsed.end());
-        std::cout << parsed << '\n';
-    }
+    // while (std::getline(test_str1, parsed, ':')) {
+    //     parsed.erase(std::remove(parsed.begin(), parsed.end(), ' '),
+    //                  parsed.end());
+    //     std::cout << parsed << '\n';
+    // }
 
     std::istringstream test_str2("sigma:\n  data/Sigma.dat");
+    std::map<std::string, std::string> data2;
+    bool isSigma = false;
     while (std::getline(test_str2, parsed)) {
-        std::cout << numInitSpaces(parsed) << "  " << parsed << '\n';
+        if (stripSpaces(parsed) == "sigma:") {
+            isSigma = true;
+            continue;
+        } else if (isSigma == true) {
+            data2["sigma"] = stripSpaces(parsed);
+        }
     }
+    std::cout << data2["sigma"] << '\n';
 }
