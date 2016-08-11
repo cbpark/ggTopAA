c033e32b892ef186d95ca07d1f1ef8f22190a9d8
cbpark <cbpark@gmail.com>
2016-08-11 16:58:37 +0900

    Use GetRandom instead of FillRandom

--- src/histogram.cc
+++ src/histogram.cc
@@ -31,7 +31,7 @@ void HistObjs::fill_bg(const InputData &data, shared_ptr<Info> info) {
             while (*f >> content) {
                 hist->Fill(content);
                 ++n_entries;
-                if (r.includes(content)) { ++n; }
+                if (content > r.low() && content < r.up()) { ++n; }
             }
         }
         if (bg.first == "direct") {
@@ -61,9 +61,18 @@ shared_ptr<TH1D> HistObjs::pseudo_experiment(const Info &info) const {
     const auto r = bg_->range();
     auto h_pd = std::make_shared<TH1D>("hPD", "Pseudo data", bg_->num_bins(),
                                        r.low(), r.up());
-    h_pd->FillRandom(sig_->hist().get(), info.n_sig());
-    h_pd->FillRandom(bg_->hist().get(), info.n_bg());
-    // h_pd->SetMinimum(0.0);
+    // h_pd->FillRandom(sig_->hist().get(), info.n_sig());
+    // h_pd->FillRandom(bg_->hist().get(), info.n_bg());
+    const int n_sig = info.n_sig();
+    for (int i = 0; i != n_sig; ++i) {
+        h_pd->Fill(sig_->hist()->GetRandom(), 1.0);
+    }
+    const int n_bg = info.n_bg();
+    for (int i = 0; i != n_bg; ++i) {
+        h_pd->Fill(bg_->hist()->GetRandom(), 1.0);
+    }
+
+    h_pd->SetMinimum(0.0);
     return h_pd;
 }
 }  // namespace gg2aa
