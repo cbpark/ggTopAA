6c80649424d93a680e525a2b3a6e79b897159eb9
cbpark <cbpark@gmail.com>
2016-08-21 02:02:00 +0900

    Set CC and CFLAGS in Makefile

--- cephes/module.mk
+++ cephes/module.mk
@@ -6,7 +6,7 @@ LIB      += $(LIBDIR)/libmd.a
 HEAD     += $(CEPHES_DIR)/cephes_hyp21f.h
 CXXFLAGS += -I$(CEPHES_DIR)
 
-$(LIBDIR)/libmd.a: CFLAGS += -g -O2 -fno-builtin -fPIC -I$(CEPHES_DIR)
+$(LIBDIR)/libmd.a: CFLAGS += -fno-builtin -fPIC -I$(CEPHES_DIR)
 $(LIBDIR)/libmd.a: $(CEPHES_OBJS)
 	$(AR) $@ $^
 	ranlib $@
